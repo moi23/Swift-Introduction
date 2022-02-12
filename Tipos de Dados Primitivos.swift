f26ffaef42310a899ca05a81c048d5e0ba85b48b
Moisés Vilas Boas <moisesboas@gmail.com>
2022-02-12 14:37:20 -0300

    feat: tipos de dados primitivos

---
+++ Tipos de Dados Primitivos.swift
@@ -0,0 +1,15 @@
+// Printing
+print("Hello World")
+
+
+// Variables
+var nome = "Moisés"; // Pode ser reatribuido
+let name = "Ramses"; // Não pode ser atribuido == (const)
+
+// Ex:
+nome = "Ramses2"
+print(nome) // output: "Ramses2"
+
+
+
+
