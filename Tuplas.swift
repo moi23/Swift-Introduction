4f72bca46218c95550c03257c722992ab6b07a4b
Moisés Vilas Boas <moisesboas@gmail.com>
2022-02-12 21:07:51 -0300

    feat: desestruct in tuplas

--- Tuplas.swift
+++ Tuplas.swift
@@ -1,7 +1,7 @@
 // Tuplas são nada mais que forams de armazenar informações de um contexto especifico.
 
-var username = "Moisés";
-var password = "123456";
+// var username = "Moisés";
+// var password = "123456";
 
 // let coords: (Double, Double) = (23.4, 54.22);
 // coords.0
@@ -13,6 +13,16 @@ var password = "123456";
 
 // ou também
 
-let coordss = (lat: 23.2, lng:23.2);
-print(coordss.0) // expected outpupt is - 23.2
-print(coordss.lat) // expected output is - 23.2 (também funciona igual...)
+// let coordss = (lat: 23.2, lng:23.2);
+// print(coordss.0) // expected outpupt is - 23.2
+// print(coordss.lat) // expected output is - 23.2 (também funciona igual...)
+
+let camera = (x:10, y:20, z:1);
+
+// Desesctruct em Tuplas
+// ex:
+
+let (x, y, z) = camera;
+print(x) // expected output is - 10;
+print(y) // expected output is - 20;
+print(z) // expected output is - 1;
