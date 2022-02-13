19de5f58353e86ef2cd3c4c5c19d936620ba7caa
Moisés Vilas Boas <moisesboas@gmail.com>
2022-02-12 21:30:04 -0300

    FEAT: Array Append

--- Arrays.swift
+++ Arrays.swift
@@ -1,6 +1,7 @@
 // ARRAYS OU COLEÇÕES DE DADOS OU LISTA DE DADOS
+// ----------------------------------------
 
-
+// Declarando, Herdando e Listando o elemento herdado
 let userNames:[Any] = [1,2,3,4,5,6];
 
 var test = userNames;
@@ -13,3 +14,15 @@ print(test[0])
 print(test[1])
 
 print(test)
+
+// ----------------------------------------
+// reatribuindo
+// metodo nativo append adiciona um novo valor a uma variavel var
+
+// nomeArray.append("Novo valor tipo dados primitivos")
+
+var  userNamess:[String] = ["Moisés"]
+userNamess.append("Belle")
+
+print(userNamess) // expected output is ["Moisés", "Anabelle"]
+
